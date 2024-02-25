c6596897621fbb063650098edc19fdc55069b65a
Shilo <94719260+ShiloBuchnik@users.noreply.github.com>
2024-02-26 01:16:35 +0200

    CRUD API is done + half of basic API

--- Solution.py
+++ Solution.py
@@ -43,11 +43,34 @@ def createTables():
                      "apartment_id INTEGER PRIMARY KEY NOT NULL CHECK(apartment_id > 0),"
                      "owner_id INTEGER NOT NULL CHECK(owner_id > 0),"
                      "FOREIGN KEY (owner_id) REFERENCES Owners(owner_id) ON DELETE CASCADE,"
-                     "FOREIGN KEY (apartment_id) REFERENCES Apartments(apartment_id));"
+                     "FOREIGN KEY (apartment_id) REFERENCES Apartments(apartment_id) ON DELETE CASCADE);"
 
 
                      # "FOREIGN KEY (owner_id) REFERENCES Owners(owner_id)"
                      # "ON DELETE CASCADE)"
+					 
+					 
+					"CREATE TABLE Reviews( "
+					"cust_id INTEGER NOT NULL CHECK(cust_id > 0),"
+					"apartment_id INTEGER NOT NULL CHECK(apartment_id > 0),"
+					"PRIMARY KEY (cust_id, apartment_id),"
+					"review_date DATE NOT NULL,"
+					"rating INTEGER NOT NULL CHECK(Rating >= 1 AND Rating <= 10),"
+					"review_text TEXT NOT NULL,"
+					"FOREIGN KEY (cust_id) REFERENCES Customers(cust_id) ON DELETE CASCADE,"
+                    "FOREIGN KEY (apartment_id) REFERENCES Apartments(apartment_id) ON DELETE CASCADE);"
+					 
+					"CREATE TABLE Reserves( "
+					"cust_id INTEGER NOT NULL CHECK(cust_id > 0),"
+					"apartment_id INTEGER NOT NULL CHECK(apartment_id > 0),"
+					"start_date DATE NOT NULL,"
+					"PRIMARY KEY (cust_id, apartment_id, start_date),"
+					"end_date DATE NOT NULL,"
+					"CHECK (end_date > start_date),"
+					"total_price INTEGER NOT NULL CHECK(total_price > 0),"
+					"FOREIGN KEY (cust_id) REFERENCES Customers(cust_id) ON DELETE CASCADE,"
+                    "FOREIGN KEY (apartment_id) REFERENCES Apartments(apartment_id) ON DELETE CASCADE);"
+
                      "COMMIT;")
 
         conn.commit()
@@ -63,7 +86,7 @@ def clearTables():
     conn = None
     try:
         conn = Connector.DBConnector()
-        conn.execute("TRUNCATE Owners, Apartments, Customers, Owns")
+        conn.execute("TRUNCATE Owners, Apartments, Customers, Owns, Reviews, Reserves")
         conn.commit()
 
     except Exception as e:
@@ -82,6 +105,8 @@ def dropTables():
                      "DROP TABLE IF EXISTS Apartments CASCADE;"
                      "DROP TABLE IF EXISTS Customers CASCADE;"
                      "DROP TABLE IF EXISTS Owns CASCADE;"
+					 "DROP TABLE IF EXISTS Reviews CASCADE;"
+					 "DROP TABLE IF EXISTS Reserves CASCADE;"
 
                      "COMMIT")
         conn.commit()
@@ -102,7 +127,7 @@ def add_owner(owner: Owner) -> ReturnValue:
         conn = Connector.DBConnector()
         query = sql.SQL("INSERT INTO Owners(owner_id, owner_name) "
                         "VALUES ({owner_id},{owner_name})").format(owner_id=owner_id, owner_name=owner_name)
-        rows, _ = conn.execute(query)
+        rows, _ = conn.execute(query) # TODO: why do you assign into 'rows' here?
         conn.commit()
     except DatabaseException.NOT_NULL_VIOLATION as e:
         return ReturnValue.BAD_PARAMS
@@ -125,7 +150,7 @@ def get_owner(owner_id: int) -> Owner:
     conn = None
     try:
         conn = Connector.DBConnector()
-        rows_effected, result = conn.execute(
+        rows_effected, result = conn.execute( # TODO: why do you assign into 'rows_effected' here?
             "SELECT * FROM Owners WHERE Owners.owner_id = {owner_id}".format(owner_id=owner_id))
         conn.commit()
 
@@ -134,7 +159,7 @@ def get_owner(owner_id: int) -> Owner:
 
     finally:
         conn.close()
-        if result.rows:
+        if result.rows: # TODO: why is this in the 'finally'? Isn't that going to execute regardless of an exception?
             return Owner(result.rows[0][0], result.rows[0][1])
         return Owner.bad_owner()
 
@@ -150,6 +175,8 @@ def delete_owner(owner_id: int) -> ReturnValue:
 
     except Exception as e:
         return ReturnValue.ERROR
+
+	#TODO: what about BAD_PARAMS?
 
     finally:
         conn.close()
@@ -175,7 +202,7 @@ def add_apartment(apartment: Apartment) -> ReturnValue:
             apartment_id=apartment_id,
             address=address, city=city,
             country=country, size=size)
-        rows, _ = conn.execute(query)
+        rows, _ = conn.execute(query) # TODO: why do you assign into 'rows' here?
         conn.commit()
     except DatabaseException.NOT_NULL_VIOLATION as e:
         return ReturnValue.BAD_PARAMS
@@ -207,7 +234,7 @@ def get_apartment(apartment_id: int) -> Apartment:
 
     finally:
         conn.close()
-        if result.rows:
+        if result.rows: # TODO: why is this in the 'finally'? Isn't that going to execute regardless of an exception?
             return Apartment(result.rows[0][0], result.rows[0][1], result.rows[0][2], result.rows[0][3],
                              result.rows[0][4])
         return Apartment.bad_apartment()
@@ -224,6 +251,8 @@ def delete_apartment(apartment_id: int) -> ReturnValue:
 
     except Exception as e:
         return ReturnValue.ERROR
+
+	# TODO: what about BAD_PARAMS?
 
     finally:
         conn.close()
@@ -275,7 +304,7 @@ def get_customer(customer_id: int) -> Customer:
 
     finally:
         conn.close()
-        if result.rows:
+        if result.rows: # TODO: why is this in the 'finally'? Isn't that going to execute regardless of an exception?
             return Customer(result.rows[0][0], result.rows[0][1])
         return Customer.bad_customer()
 
@@ -292,6 +321,8 @@ def delete_customer(customer_id: int) -> ReturnValue:
     except Exception as e:
         return ReturnValue.ERROR
 
+	# TODO: what about BAD_PARAMS?
+
     finally:
         conn.close()
 
@@ -303,32 +334,134 @@ def delete_customer(customer_id: int) -> ReturnValue:
 
 def customer_made_reservation(customer_id: int, apartment_id: int, start_date: date, end_date: date,
                               total_price: float) -> ReturnValue:
-    # TODO: implement
-    # Shilo
-    pass
+	conn = None
+	try:
+		conn = Connector.DBConnector()
+
+		# A bit complicated: there isn't a FROM clause here.
+		# Basically, 'WHERE NOT EXISTS' is true when there's no overlapping, and in that case, 'select' will just create a row on the fly
+		# 'WHERE NOT EXISTS' is false when there is overlapping, and in that case the entire subquery will return an empty relation
+		rows_effected, _ = conn.execute("INSERT INTO Reserves "
+						"SELECT {customer_id},{apartment_id},{start_date},{end_date},{total_price}"
+						"WHERE NOT EXISTS("
+						"SELECT 1 FROM Reserves r WHERE r.apartment_id = {apartment_id} AND (r.start_date, r.end_date) OVERLAPS ({start_date}, {end_date}) )"
+							.format(customer_id=customer_id, apartment_id=apartment_id, start_date=start_date, end_date=end_date, total_price=total_price))
+		conn.commit()
+
+	except DatabaseException.NOT_NULL_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.CHECK_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	#except DatabaseException.UNIQUE_VIOLATION as e: TODO: for Shilo, think if this table should have keys.
+	#	return ReturnValue.ALREADY_EXISTS
+	#except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+	#	return ReturnValue.ALREADY_EXISTS
+	except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+		return ReturnValue.NOT_EXISTS
+	except DatabaseException.ConnectionInvalid as e:
+		return ReturnValue.ERROR
+	except Exception as e:
+		return ReturnValue.ERROR
+	finally:
+		conn.close()
+
+	if rows_effected == 0: # In case of dates overlapping
+		return ReturnValue.BAD_PARAMS
+
+	return ReturnValue.OK
 
 
 def customer_cancelled_reservation(customer_id: int, apartment_id: int, start_date: date) -> ReturnValue:
-    # TODO: implement
-    # Shilo
+	conn = None
+	try:
+		conn = Connector.DBConnector()
+		rows_effected, _ = (conn.execute("DELETE FROM Reserves "
+						"WHERE Reserves.cust_id = {customer_id} AND Reserves.apartment_id = {apartment_id} AND Reserves.start_date = {start_date}")
+								 .format(customer_id=customer_id,apartment_id=apartment_id,start_date=start_date.strftime('%Y-%m-%d')))
+		conn.commit()
 
-    pass
+	except DatabaseException.NOT_NULL_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.CHECK_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+		return ReturnValue.NOT_EXISTS
+	except DatabaseException.ConnectionInvalid as e:
+		return ReturnValue.ERROR
+	except Exception as e:
+		return ReturnValue.ERROR
+
+	finally:
+		conn.close()
+
+	if rows_effected == 0:
+		return ReturnValue.NOT_EXISTS
+
+	return ReturnValue.OK
 
 
 def customer_reviewed_apartment(customer_id: int, apartment_id: int, review_date: date, rating: int,
                                 review_text: str) -> ReturnValue:
-    # TODO: implement
-    # Shilo
+	conn = None
+	try:
+		conn = Connector.DBConnector()
 
-    pass
+		# Same shtick as with 'customer_made_reservation()'
+		# 'WHERE EXISTS' is true when there's a reservation that ended before 'review_date', and in that case, 'select' will just create a row on the fly
+		# 'WHERE EXISTS' is false otherwise, and in that case the entire subquery will return an empty relation
+		(conn.execute("INSERT INTO Reviews "
+						"SELECT {customer_id},{apartment_id},{review_date},{rating},{review_text}"
+					  	"WHERE EXISTS("
+					  	"SELECT 1 FROM Reserves r WHERE r.cust_id = {customer_id} AND r.apartment_id = {apartment_id} AND r.end_date < {review_date}")
+								 .format(customer_id=customer_id,apartment_id=apartment_id,review_date=review_date.strftime('%Y-%m-%d'),rating=rating,review_text=review_text))
+		conn.commit()
+
+	except DatabaseException.NOT_NULL_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.CHECK_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+		return ReturnValue.NOT_EXISTS
+	except DatabaseException.UNIQUE_VIOLATION as e:
+		return ReturnValue.ALREADY_EXISTS
+	except DatabaseException.ConnectionInvalid as e:
+		return ReturnValue.ERROR
+	except Exception as e:
+		return ReturnValue.ERROR
+
+	finally:
+		conn.close()
+
+	return ReturnValue.OK
 
 
-def customer_updated_review(customer_id: int, apartmetn_id: int, update_date: date, new_rating: int,
+def customer_updated_review(customer_id: int, apartment_id: int, update_date: date, new_rating: int,
                             new_text: str) -> ReturnValue:
-    # TODO: implement
-    # Shilo
+	conn = None
+	try:
+		conn = Connector.DBConnector()
+		rows_effected, _ = (conn.execute("UPDATE Reviews "
+						"SET rating={new_rating}, review_text={new_text}"
+						"WHERE cust_id={customer_id} AND apartment_id={apartment_id} AND review_date < {update_date}")
+								 .format(new_rating=new_rating,new_text=new_text,customer_id=customer_id,apartment_id=apartment_id,update_date=update_date.strftime('%Y-%m-%d')))
+		conn.commit()
 
-    pass
+	except DatabaseException.NOT_NULL_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.CHECK_VIOLATION as e:
+		return ReturnValue.BAD_PARAMS
+	except DatabaseException.ConnectionInvalid as e:
+		return ReturnValue.ERROR
+	except Exception as e:
+		return ReturnValue.ERROR
+
+	finally:
+		conn.close()
+
+	if rows_effected == 0:
+		return ReturnValue.NOT_EXISTS
+
+	return ReturnValue.OK
 
 
 def owner_owns_apartment(owner_id: int, apartment_id: int) -> ReturnValue:
@@ -405,8 +538,8 @@ def get_apartment_owner(apartment_id: int) -> Owner:
 
     finally:
         conn.close()
-        if result.rows:
-            return Owner(result.rows[0][0], result.rows[0][1])
+        if result.rows: # TODO: why is this in the 'finally'? Isn't that going to execute regardless of an exception?
+            return Owner(result.rows[0][0], result.rows[0][1]) # TODO: wait, why are you addressing result using 'rows'?
         return Owner.bad_owner()
 
 
@@ -434,8 +567,6 @@ def get_owner_apartments(owner_id: int) -> List[Apartment]:
 
 
 
-
-
 # ---------------------------------- BASIC API: ----------------------------------
 
 def get_apartment_rating(apartment_id: int) -> float:
@@ -449,13 +580,46 @@ def get_owner_rating(owner_id: int) -> float:
 
 
 def get_top_customer() -> Customer:
-    # TODO: implement
-    pass
+	conn = None
+	try:
+		conn = Connector.DBConnector()
+		# We group 'Reserves' by 'cust_id', and then sort it:
+		# First by group's count in descending order (so bigger is first), and if there's a tie - then by 'cust_id' in ascending order (so smaller is first)
+		# Then we limit only to the first tuple (we only want the top customer)
+		_, result = conn.execute("SELECT cust_id"
+									"FROM Reserves"
+									"GROUP BY cust_id"
+									"ORDER BY COUNT(*) DESC, cust_id"
+									"LIMIT 1")
+		conn.commit()
+
+		return Customer(result[0]['cust_id'], result[0]['cust_name'])
+
+	except Exception as e:
+		return ReturnValue.ERROR
+
+	finally:
+		conn.close()
 
 
 def reservations_per_owner() -> List[Tuple[str, int]]:
-    # TODO: implement
-    pass
+	conn = None
+	try:
+		conn = Connector.DBConnector()
+		# We want num_of_reservations for *all* owners, not just ones with actual reservations.
+		# Because of that, we first use right outer join, and only then we group by 'owner_id'
+		_, result = conn.execute("SELECT owner_id, COUNT(*) AS num_of_reservations"
+									"FROM Reserves r RIGHT OUTER JOIN Owns o ON r.apartment.id = o.apartment"
+									"GROUP BY o.owner_id")
+		conn.commit()
+
+		return result
+
+	except Exception as e:
+		return ReturnValue.ERROR
+
+	finally:
+		conn.close()
 
 
 # ---------------------------------- ADVANCED API: ----------------------------------
@@ -478,3 +642,8 @@ def profit_per_month(year: int) -> List[Tuple[int, float]]:
 def get_apartment_recommendation(customer_id: int) -> List[Tuple[Apartment, float]]:
     # TODO: implement
     pass
+
+dropTables()
+createTables()
+add_customer(Customer(123, "David"))
+add_customer(Customer(222, "Yossi"))
